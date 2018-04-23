1218412e25456d3d8486be7b57bce4f8d0d5cbb2
Richard Ellery <raellery@gmail.com>
2018-04-24 00:14:44 +0900

    Add TaskInput component and flex container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class TaskInput extends Component {
+  render() {
+    return (
+      <form>
+        <input  className="input" placeholder="Enter task..." type ="text"  />
+      </form>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App"> 
         <i class="material-icons md-light topleft">info_outline</i>
         <i class="material-icons md-light topright">history</i>
+        <div className="flex-container">
+          <TaskInput className="input" />
+        </div>
       </div>
     );
   }
