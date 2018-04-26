62b9d40ea20b2d49541014cb772db13c44e89394
Richard Ellery <raellery@gmail.com>
2018-04-26 22:37:15 +0900

    Create basic Info component

---
+++ src/Info.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Info.css';
+
+class Info extends Component {
+  render() {
+    return (
+      <div className="info">Info Component</div>
+    );
+  }
+}
+
+export default Info;
