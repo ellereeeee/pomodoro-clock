612396235dff5f51bb1dff656f03294d062a6dd3
Richard Ellery <raellery@gmail.com>
2018-04-29 21:48:08 +0900

    Fix JSX syntax

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,9 @@ class Timer extends Component {
     return (
       <div>
         <div className="circle flex-container">
-          <i class="material-icons">arrow_drop_up</i>
+          <i className="material-icons">arrow_drop_up</i>
           <p>25:00:00</p> {/*Timer placeholder*/}
-          <i class="material-icons">arrow_drop_down</i>
+          <i className="material-icons">arrow_drop_down</i>
         </div>
         <i className="material-icons play-arrow">play_arrow</i>
       </div>
@@ -32,8 +32,8 @@ class App extends Component {
     return (
       <div className="App"> 
         <Info />
-        <i class="material-icons topleft">info_outline</i>
-        <i class="material-icons topright">history</i>
+        <i className="material-icons topleft">info_outline</i>
+        <i className="material-icons topright">history</i>
         <div className="flex-container">
           <TaskInput />
           <Timer />
