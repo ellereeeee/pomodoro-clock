b29618ee7550936dea4b6aaa87df232e4c210bd6
Richard Ellery <raellery@gmail.com>
2018-05-10 22:24:30 +0900

    Link history button to history component

--- src/PomodoroTimer.jsx
+++ src/PomodoroTimer.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './PomodoroTimer.css';
 import Info from './Info'
+import { Link } from 'react-router-dom';
 
 class TaskInput extends Component {
   render() {
@@ -39,7 +40,7 @@ class PomodoroTimer extends Component {
       <div className="PomodoroTimer"> 
         <Info visibility={this.state.toggleInfo} toggle={this.handleToggleInfo}/>
         <button className="material-icons topleft" onClick={this.handleToggleInfo}><i>info_outline</i></button>
-        <button className="material-icons topright"><i>history</i></button>
+        <i className="material-icons topright"><Link to="/history">history</Link></i>
         <div className="flex-container">
           <TaskInput />
           <Timer />
