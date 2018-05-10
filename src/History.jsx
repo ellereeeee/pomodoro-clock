54be64491817df48429ac996535312c759af33ab
Richard Ellery <raellery@gmail.com>
2018-05-10 22:25:01 +0900

    Add simple History jsx and css files

---
+++ src/History.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './History.css';
+
+class History extends Component {
+  render() {
+    return (
+      <p>test</p>
+    );
+  }
+}
+
+export default History;
