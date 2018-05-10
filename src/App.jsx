1f5c8c6bbe514d4eae09a4c9bcbb65f26a6da059
Richard Ellery <raellery@gmail.com>
2018-05-10 22:23:57 +0900

    Add switch and route components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import PomodoroTimer from './PomodoroTimer';
 import History from './History';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <PomodoroTimer />
+      <Switch>
+        <Route exact path="/" component={PomodoroTimer}/>
+        <Route exact path="/history" component={History}/>
+      </Switch>
     )
   }
 }
