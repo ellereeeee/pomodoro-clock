9866f43fb336fd7977bb307667a7beee5e27dab8
Richard Ellery <raellery@gmail.com>
2018-05-10 22:07:38 +0900

    Rename old App.jsx to PomodoroTimer.jsx, add new App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
-import Info from './Info'
-
-class TaskInput extends Component {
-  render() {
-    return (
-      <form>
-        <input  className="input" placeholder="Enter task..." type ="text"  />
-      </form>
-    );
-  }
-}
-
-class Timer extends Component {
-  render() {
-    return (
-      <div>
-        <div className="circle flex-container">
-          <i className="material-icons">arrow_drop_up</i>
-          <p>25:00:00</p> {/*Timer placeholder*/}
-          <i className="material-icons">arrow_drop_down</i>
-        </div>
-        <i className="material-icons play-arrow">play_arrow</i>
-      </div>
-    );
-  }
-}
+import PomodoroTimer from './PomodoroTimer';
+import History from './History';
 
 class App extends Component {
-  state = { toggleInfo: false};
-  handleToggleInfo = () => {
-    !this.state.toggleInfo
-      ? this.setState({ toggleInfo: true })
-      : this.setState({ toggleInfo: false });
-  };
   render() {
     return (
-      <div className="App"> 
-        <Info visibility={this.state.toggleInfo} toggle={this.handleToggleInfo}/>
-        <button className="material-icons topleft" onClick={this.handleToggleInfo}><i>info_outline</i></button>
-        <button className="material-icons topright"><i>history</i></button>
-        <div className="flex-container">
-          <TaskInput />
-          <Timer />
-        </div>
-      </div>
-    );
+      <PomodoroTimer />
+    )
   }
 }
 
---
+++ src/PomodoroTimer.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './PomodoroTimer.css';
+import Info from './Info'
+
+class TaskInput extends Component {
+  render() {
+    return (
+      <form>
+        <input  className="input" placeholder="Enter task..." type ="text"  />
+      </form>
+    );
+  }
+}
+
+class Timer extends Component {
+  render() {
+    return (
+      <div>
+        <div className="circle flex-container">
+          <i className="material-icons">arrow_drop_up</i>
+          <p>25:00:00</p> {/*Timer placeholder*/}
+          <i className="material-icons">arrow_drop_down</i>
+        </div>
+        <i className="material-icons play-arrow">play_arrow</i>
+      </div>
+    );
+  }
+}
+
+class PomodoroTimer extends Component {
+  state = { toggleInfo: false};
+  handleToggleInfo = () => {
+    !this.state.toggleInfo
+      ? this.setState({ toggleInfo: true })
+      : this.setState({ toggleInfo: false });
+  };
+  render() {
+    return (
+      <div className="PomodoroTimer"> 
+        <Info visibility={this.state.toggleInfo} toggle={this.handleToggleInfo}/>
+        <button className="material-icons topleft" onClick={this.handleToggleInfo}><i>info_outline</i></button>
+        <button className="material-icons topright"><i>history</i></button>
+        <div className="flex-container">
+          <TaskInput />
+          <Timer />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PomodoroTimer;
