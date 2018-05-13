2752c646ad7be14ca49e31421b6f4c890d45284f
Richard Ellery <raellery@gmail.com>
2018-05-13 23:37:58 +0900

    Add TaskItem and History compoments with placeholders

--- src/History.jsx
+++ src/History.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 import './History.css';
 
+class TaskItem extends Component {
+  render () {
+    return (
+      <div className="History-item">
+        <i class="material-icons">alarm_on</i>
+        <p>Work on Pomodoro for 25</p>
+      </div>
+    );
+  }
+}
+
 class History extends Component {
   render() {
     return (
-      <p>test</p>
+      <div className="History">
+        <div className="test">
+          <h3 className="topleft">History</h3>
+          <i className="material-icons topright">arrow_back</i>
+        </div>
+        <div className="History-flex-container">
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+        </div>
+      </div>
     );
   }
 }
