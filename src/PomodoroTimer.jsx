521f215b9a7c35c226e4264fcb967cf25b832cd3
Richard Ellery <raellery@gmail.com>
2018-06-05 22:15:33 +0900

    Make state reset properlywhen timer finishes

--- src/PomodoroTimer.jsx
+++ src/PomodoroTimer.jsx
@@ -85,8 +85,8 @@ class PomodoroTimer extends Component {
     clearInterval(this.timer);
     this.setState({ timerActive: false });
     this.setState({ offsetModifier: 1 });
-    if (this.state.time < 10) {
-      (this.state.timerType == "Pomodoro") ? this.setState({ timerType: "Rest", time: 300000 }) : this.setState({ timerType: "Pomodoro", time: 1500000 });
+    if (this.state.time < 50) {
+      (this.state.timerType == "Pomodoro") ? this.setState({ timerType: "Rest", time: 300000 }) : this.setState({ timerType: "Pomodoro", time: 3000 });
         } else {
       this.setState({ time: this.initialStateTime });
     }
