6bf2f8c5f5aa29d42780d126bbf6ed4cfe4765fa
Richard <raellery@gmail.com>
2018-06-21 10:14:06 +0900

    Prettify App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
-import PomodoroTimer from './PomodoroTimer';
+import React, { Component } from "react";
+import PomodoroTimer from "./PomodoroTimer";
 
 class App extends Component {
   render() {
-    return (
-      <PomodoroTimer />
-    )
+    return <PomodoroTimer />;
   }
 }
 
