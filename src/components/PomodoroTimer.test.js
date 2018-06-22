68ece9f8041d3aa03aca16a9546716a3ab00831a
Richard Ellery <raellery@gmail.com>
2018-06-22 21:57:10 +0900

    Remove App.jsx, test PomodoroTimer and render it to root

--- src/components/PomodoroTimer.test.js
+++ src/components/PomodoroTimer.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import PomodoroTimer from './PomodoroTimer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<PomodoroTimer />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import PomodoroTimer from './components/PomodoroTimer';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <App />
+    <PomodoroTimer />
   , document.getElementById('root'));
 registerServiceWorker();
